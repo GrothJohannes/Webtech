afd96f5a7d131c39089ea55f48a33ff5f8835a2c
GrothJohannes <johannesgroth04@gmail.com>
2025-04-19 16:49:28 +0200

    Datei mit Spring erstellt

---
+++ src/test/java/de/htwberlin/webtech/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
